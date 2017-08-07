8d23e0ae22082a48c650e940419f97defe42be6e
Summer Saleh <summerkatherine1@gmail.com>
2017-08-07 12:08:39 -0400

    while loop and if test exercise

---
+++ src/basics/BeerSong.java
@@ -0,0 +1,29 @@
+package basics;
+
+public class BeerSong {
+
+	public static void main(String[] args) {
+
+		int beerNum = 99;
+		String word = "bottles";
+
+		while (beerNum > 0) {
+
+			if (beerNum == 1) {
+				word = "bottle";
+			}
+
+			System.out.println(beerNum + " " + word + " of beer on the wall " + beerNum + " " + word
+					+ " of beer!\n You take one down pass it around");
+
+			beerNum = beerNum - 1;
+
+			if (beerNum > 0) {
+				System.out.println(beerNum + " " + word + " of beer on the wall");
+			} else {
+				System.out.println("No more bottles of beer on the wall");
+			} //end else
+		} //end while loop
+
+	}//end main method
+}//end class
