45d7e0941395236d8b3f22fabcb95554c67d4d89
Summer <summerkatherine1@gmail.com>
2017-08-22 19:14:37 -0400

    practice for node school tutorial

---
+++ src/basics/PhraseOMatic.java
@@ -0,0 +1,41 @@
+package basics;
+
+import java.util.Scanner;
+
+public class PhraseOMatic {
+
+	public static void main(String[] args) {
+
+		// Scanner input = new Scanner(System.in);
+		//// This program makes three lists of words, then randomly picks
+		//// one word from each of the three lists, and prints out the result.
+		//
+		// String [] dogs = {"snow", "fluff", "finley"};
+		// String [] food = {"pizza", "donuts", "pasta"};
+		// String [] cities = {"columbus", "cleveland", "cincinnati"};
+		//
+		// System.out.println("Choose a number:");
+		// int choosenNumber = input.nextInt();
+		// if(choosenNumber % 2 == 0) {
+		// System.out.println("Your choosen number is even");
+		// } else {
+		// System.out.println("Your chosen number is odd");
+		// }
+		//
+		// The sum of three consecutive even numbers is 48.
+		// What is the smallest of these numbers?
+
+		int n = 48 / 3;
+
+		System.out.println(n);
+		System.out.println(n + 2);
+		System.out.println(n - 2);
+
+		int a = 72/3;
+		System.out.println(a);
+		System.out.println(a-1);
+		System.out.println(a+1);
+
+	}
+
+}
